3065f019386163a52ce41150317096736da565c4
ganesanns <ganesan.ns@cognizant.com>
2017-08-02 14:27:22 +0530

    Get pets from db

--- SpringMySql/src/main/java/com/cts/customer/controller/PetController.java
+++ SpringMySql/src/main/java/com/cts/customer/controller/PetController.java
@@ -4,7 +4,11 @@ package com.cts.customer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+import static com.cts.customer.controller.PetController.CustomerEndPoints.GET_PETS;
 import static com.cts.customer.controller.PetController.CustomerEndPoints.INDEX_URL;
 import static com.cts.customer.controller.PetController.CustomerEndPoints.ROOT;
 
@@ -17,13 +21,24 @@ public class PetController {
     private PetRepository repository;
 
     @RequestMapping(value = {ROOT, INDEX_URL})
-    public String getPets() {
+    public String addPets() {
         repository.save(new Cats("test"+System.currentTimeMillis()));
         return INDEX_PAGE;
     }
 
+
+    @RequestMapping(value = {GET_PETS})
+    public @ResponseBody List<Cats> getPets() {
+        return repository.findAll();
+    }
+
+
+
     public interface CustomerEndPoints {
         String INDEX_URL = "/index";
+        String GET_PETS = "/pets";
         String ROOT = "/";
     }
+
+
 }
